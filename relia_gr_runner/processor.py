8939c6ccdb60000de4c61b4bd69b80450606a76d
Pablo Orduña <pablo@labsland.com>
2024-09-26 15:42:10 -0700

    Rely on magic number

--- relia_gr_runner/processor.py
+++ relia_gr_runner/processor.py
@@ -173,7 +173,7 @@ class Processor:
                 file_content = file_content.replace("**RELIA_REPLACE_WITH_ADALM_PLUTO_IP_ADDRESS**", current_app.config['ADALM_PLUTO_IP_ADDRESS'])
             if current_app.config['RED_PITAYA_IP_ADDRESS']:
                 file_content = file_content.replace("**RELIA_REPLACE_WITH_RED_PITAYA_IP_ADDRESS**", current_app.config['RED_PITAYA_IP_ADDRESS'])
-                file_content = file_content.replace('"**RELIA_REPLACE_WITH_RED_PITAYA_RATE**"', current_app.config['RED_PITAYA_RATE'])
+                file_content = file_content.replace('128_95_205_60_128_95_196_55', current_app.config['RED_PITAYA_RATE'])
             # It was already compiled, no need to re-compile
             open(py_filename, 'w').write(file_content)
         else:
